1c55e85a223f24a765987a90eca587b1e742a105
artem-aksenkin <artemaksenkin@gmail.com>
2017-08-03 22:34:39 +0300

    filter script added

---
+++ day-3/filter_plugins/filter.py
@@ -0,0 +1,20 @@
+
+def get_mongo_src(args, os_family, os_ver, mongo_ver):
+    result = []
+    if os_family == 'RedHat':
+        os_family == 'rhel'
+    for arg in args:
+        count = arg.find(os_family)
+        if count != -1:
+            count = arg.find(os_ver, count+len(os_family))
+            if count != -1:
+                count = arg.find(mongo_ver, count+len(os_ver))
+                if count != -1:
+                    result.append(arg)
+    return result
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'get_mongo_src': get_mongo_src,
+        }
