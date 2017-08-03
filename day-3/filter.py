92c59265aa451da7f525b5d3a37e8e23bb01632b
artem-aksenkin <artemaksenkin@gmail.com>
2017-08-03 19:53:36 +0300

    day 3 init commit

--- day-3/filter.py
+++
@@ -1,9 +0,0 @@
-
-def get_mongo_src(args, ob, ob1, mongo_ver)
-    result = []
-    for arg in args:
-        if ob and ob1 and mongo_ver in arg:
-            result.extend(arg)
-    return result
-
-
