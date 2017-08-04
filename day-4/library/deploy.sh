e655e82692532dc8f8c875b72943fa5ab8f347a7
artem-aksenkin <artemaksenkin@gmail.com>
2017-08-04 18:26:13 +0300

    tomcat role task - renamed handler

--- day-4/library/deploy.sh
+++ day-4/library/deploy.sh
@@ -2,8 +2,7 @@
 
 # curl --silent -T $war -u $username:$password $url
 
-curl --silent -T "/home/student/cm/ansible/day-4/hello-war/target/mnt-lab.war" -u deploy:123 "127.0.0.1:8080/manager/text/deploy?path=/mnt-lab&update=true"
+curl --silent -T "/home/student/cm/ansible/day-4/hello-war/target/mnt-lab.war" -u deploy:123 "192.168.56.13/manager/text/deploy?path=/mnt-lab&update=true"
 
 #deploy: url=... war=… username=… password=…
 
-exit 0
