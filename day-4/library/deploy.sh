c91933fb82b124930621730dd96b954b5c4e73fd
artem-aksenkin <artemaksenkin@gmail.com>
2017-08-05 12:21:04 +0300

    deploy role added

--- day-4/library/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# curl --silent -T $war -u $username:$password $url
-
-curl --silent -T "/home/student/cm/ansible/day-4/hello-war/target/mnt-lab.war" -u deploy:123 "192.168.56.13/manager/text/deploy?path=/mnt-lab&update=true"
-
-#deploy: url=... war=… username=… password=…
-
